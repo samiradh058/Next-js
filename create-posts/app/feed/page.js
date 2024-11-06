1eeb6907ab861ac2cd53e2270e41404d71125d9f
samiradh058 <adhikarisamir68@gmail.com>
2024-11-06 12:42:05 +0545

    Optimization of images and metadata

--- create-posts/app/feed/page.js
+++ create-posts/app/feed/page.js
@@ -1,5 +1,15 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// For dynamic metadata
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numOfPosts = posts.length;
+  return {
+    title: `Browse all ${numOfPosts} posts`,
+    description: "Browse all out posts",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
