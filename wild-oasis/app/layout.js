38a2de74c20e33ed9183bfc04b533d5d5f056c11
samiradh058 <adhikarisamir68@gmail.com>
2024-12-06 23:53:11 +0545

    Client side wild oasis app

---
+++ wild-oasis/app/layout.js
@@ -0,0 +1,34 @@
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+export const metadata = {
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description: "Luxurious cabin hotel, located in the heart of Pokhara city",
+};
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
