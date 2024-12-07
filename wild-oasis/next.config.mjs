e4713d17b482cbceb23b6b7390b1447385fb24cc
samiradh058 <adhikarisamir68@gmail.com>
2024-12-07 22:23:38 +0545

    Cache error not-found

--- wild-oasis/next.config.mjs
+++ wild-oasis/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "jntirxctqfllvyqaqmdj.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+  // For static deployment
+  // output: "export",
+};
 
 export default nextConfig;
