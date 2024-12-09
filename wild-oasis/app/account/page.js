e179d2eec7feff6dda56c9a446752423e284c0c2
samiradh058 <adhikarisamir68@gmail.com>
2024-12-09 16:30:46 +0545

    Next authentication and middleware

--- wild-oasis/app/account/page.js
+++ wild-oasis/app/account/page.js
@@ -1,11 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
 };
 
-export default function Account() {
+export default async function Account() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Samir
+      Welcome, {firstName}
     </h2>
   );
 }
